5f303e25c8a2da8bfc87ba531cd90b3ca5994bce
Vorobeyyyyyy <mrskilkchat@gmail.com>
2020-12-22 14:30:15 +0300

    comparatorTest

--- src/com/vorobyev/shape/observer/PyramidEvent.java
+++ src/com/vorobyev/shape/observer/PyramidEvent.java
@@ -5,7 +5,7 @@ import com.vorobyev.shape.entity.Pyramid;
 import java.util.EventObject;
 
 public class PyramidEvent extends EventObject {
-    public PyramidEvent(Object source) {
+    public PyramidEvent(Pyramid source) {
         super(source);
     }
 
